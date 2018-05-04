e27fd1e708d14fce9a83891464a383ddda030977
Jasper Jenkins <ownyourfate@yahoo.com>
2018-05-04 10:25:46 -0600

    update

--- player-controller.js
+++ player-controller.js
@@ -3,46 +3,58 @@ function PlayersController() {
     //PRIVATE
     var playersService = new PlayersService(drawPlayerSelect)
     this.getPlayers = function getPlayers(e) {
+      debugger
         e.preventDefault();
         var firstname = e.target.firstname.value;
-        var players = playersService.getPlayers; //after get music by artist returns what are you doing with the objects?
-        document.getElementById("playerPick").innerText=players
+      debugger
+        // var players = playersService.getPlayers; //after get music by artist returns what are you doing with the objects?
+        // document.getElementById("playerPick").innerText=players
+      //  drawPlayerSelect()
       }
 
     function drawPlayerSelect(chars) {
         var template = "<h1>Choose Wisely</h1>";
         console.log(chars)
-        for (var i = 0; i < 5; i++) {   ////chars.length
-           
+        for (var i = 0; i < 3; i++) {   ////chars.length
             var char = chars[i];
-            if(char.firstname==""){
-                char.splice(i, 1)
-            }else{
             template += `
                 <div>
-                <p>Name: ${char.firstname}</p>
-                <img src="${char.photo}" alt ="${char.firstname}">
-                <p></p>
+                 <img src="${char.photo}" alt ="${char.firstname}">
+                 <p>Name: ${char.firstname}</p>
+                 <button onclick="app.controller.playersController.addToTeam(${char.id})">Add to team</button>
+                </div>
+            `
+           }
+        document.getElementById("playerPick").innerHTML = template;
+    }
 
-                
-                <button onclick="app.controllers.playersController.addToTeam(${char.id})">Add to team</button>
+    function drawPlayerOnTeam(chars) {
+        var template = "<h1>Team Assemled</h1>";
+        console.log(chars)
+        // for (var i = 0; i < 3; i++) {   ////chars.length
+            var char = chars[i];
+            template += `
+                <div>
+                 <img src="${char.photo}" alt ="${char.firstname}">
+                 <p>Name: ${char.firstname}</p>
+                 <button onclick="app.controller.playersController.myTeam(${char.id})">Add to team</button>
                 </div>
             `
             debugger 
-            }
-        }
+     //       }
         document.getElementById("playerPick").innerHTML = template;
     }
+
     //PUBLIC
 
    // document.getElementById('myTeam').innerHTML = playersService.getPlayers()
-
+debugger
 
 
 
 
 this.addToTeam = function addToTeam(id) {
- playersService.addMyTeam(id, drawMyTeam);
+ playersService.addToTeam(id, drawPlayerOnTeam);
 };
 
 // this.removeFromTeam = function removeFromTeam(id) {
--- player-service.js
+++ player-service.js
@@ -1,65 +1,69 @@
-function PlayersService(callback){
-    var playersData = []
-    var myTeam = []
-  
-    //...
-    //...
-     function loadPlayersData(){
-       //check if the player already has a copy of the NFL playersData
-       var localData = localStorage.getItem('playersData');
-      //if they do, pull from there
-       if(localData){
-           playersData = JSON.parse(localData);
-           //return will short-circuit the loadPlayersData function
-           //this will prevent the code below from ever executing
-           return callback(playersData)
-       }
-       //if not go get that data
-       var url = "https://bcw-getter.herokuapp.com/?url=";
-       var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-       var apiUrl = url + encodeURIComponent(endpointUri);
- 
-         $.getJSON(apiUrl, function(data){
-           playersData = data.body.players;
-           console.log('Player Data Ready')
-           console.log('Writing Player Data to localStorage')
-           localStorage.setItem('playersData', JSON.stringify(playersData))
-           console.log('Finished Writing Player Data to localStorage')
-           callback(playersData)
-         });
-     } 
-     
-this.getPlayers = function getPlayers(){  // retrieve an array of players
-    return playersData
-}
-this.getMyTeam = function getMyTeam(){  // retrieve an array of my team members
-    return myTeam
-}
+function PlayersService(callback) {
+  var playersData = []
+  var myTeam = []
 
-this.addMyTeam = function addMyTeam(newCharacterId, cb) { ///////---- this may need to change from ID to another searchable element
+  //...
+  //...
+  function loadPlayersData() {
+    //check if the player already has a copy of the NFL playersData
+    var localData = localStorage.getItem('playersData');
+    //if they do, pull from there
+    if (localData) {
+      playersData = JSON.parse(localData);
+      //return will short-circuit the loadPlayersData function
+      //this will prevent the code below from ever executing
+      return callback(playersData)
+    }
+    //if not go get that data
+    var url = "https://bcw-getter.herokuapp.com/?url=";
+    var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+    var apiUrl = url + encodeURIComponent(endpointUri);
+
+    $.getJSON(apiUrl, function (data) {
+      playersData = data.body.players;
+      console.log('Player Data Ready')
+      console.log('Writing Player Data to localStorage')
+      localStorage.setItem('playersData', JSON.stringify(playersData))
+      console.log('Finished Writing Player Data to localStorage')
+      callback(playersData)
+    });
+  }
+
+  this.getPlayers = function getPlayers() {  // retrieve an array of players
+    return playersData
+  }
+  this.getMyTeam = function getMyTeam() {  // retrieve an array of my team members
+    return myTeam
+  }
+
+  this.addToTeam = function addToTeam(newCharacterId, cb) { ///////---- this may need to change from ID to another searchable element
     //Find itterates over an array, looking at each object in the array, passing it to a function, that function must return true or false to determine if that object is the one you are looking for. It will return the first instance of that thing
-    var newMember = playersData.find(function(char){
+    var newMember = playersData.find(function (char) {
       return char.id == newCharacterId
     })
     myTeam.push(newMember)
     cb(myTeam);
   };
-  
-  this.removeFromTeam = function removeFromTeam(removeId, draw) {
-    debugger
-    // This is for removing characters from myTeam
-    var removeMember = myTeam.find(function (char) {
-      return char.id == removeId
-    })
+this.searchForPlayers = function searchForPlayers(){
 
-    //indexOf itterates over an array to find the element it was passed and returns the index, if it doesnt find it it will return -1
-    var index = myTeam.indexOf(removeMember)
-    //splice removes object from array
-    myTeam.splice(index, 1)
+}
 
-    draw(myTeam)
 
-  };
+  // this.removeFromTeam = function removeFromTeam(removeId, draw) {
+  //   debugger
+  //   // This is for removing characters from myTeam
+  //   var removeMember = myTeam.find(function (char) {
+  //     return char.id == removeId
+  //   })
 
-loadPlayersData(); //call the function above every time we create a new service
+  //   //indexOf itterates over an array to find the element it was passed and returns the index, if it doesnt find it it will return -1
+  //   var index = myTeam.indexOf(removeMember)
+  //   //splice removes object from array
+  //   myTeam.splice(index, 1)
+
+  //   draw(myTeam)
+
+  // };
+
+  loadPlayersData(); //call the function above every time we create a new service
 }
